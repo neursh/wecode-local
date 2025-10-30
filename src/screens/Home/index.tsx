aecadccece19af3e613f630a34fb1f008aac77bb
neursh <31346263+neursh@users.noreply.github.com>
2025-10-30 20:01:50 +0700

    Add a slick design

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,8 @@ import Greet from './content/Greet';
 
 export default function Home() {
   return (
-    <section className="w-full h-dvh">
-      <section className="pl-12 pr-12 pt-12">
+    <section className="w-full h-dvh p-4">
+      <section className="w-full h-full overflow-scroll p-8 outline outline-[white]/40 rounded-2xl">
         <Greet />
         <DisplayAssignments />
       </section>
