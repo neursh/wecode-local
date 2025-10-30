7cf1c44ee6f59c28fcc21ea23922217c3dd34149
neursh <31346263+neursh@users.noreply.github.com>
2025-10-30 16:40:05 +0700

    Put it together

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from 'react';
-import { HomeContext } from './context';
+import DisplayAssignments from './content/DisplayAssignments';
+import Greet from './content/Greet';
 
 export default function Home() {
-  useLayoutEffect(() => {
-    HomeContext.parseAssignments();
-  }, []);
-
-  return <></>;
+  return (
+    <section className="w-full h-dvh">
+      <section className="pl-12 pr-12 pt-12">
+        <Greet />
+        <DisplayAssignments />
+      </section>
+    </section>
+  );
 }
