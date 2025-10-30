b6dabeace740ade02e97067f800064eeada1cec1
neursh <31346263+neursh@users.noreply.github.com>
2025-10-30 20:02:55 +0700

    Add teh button to exit profile

--- src/screens/Home/content/Greet.tsx
+++ src/screens/Home/content/Greet.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { GlobalContext } from '../../../context';
 
 export default function Greet() {
   const greets = useMemo(
@@ -11,7 +12,7 @@ export default function Greet() {
       'Wassup?',
       'Greetings!',
       'Hello there!',
-      ':3',
+      ':3', // Super rare
     ],
     []
   );
@@ -20,5 +21,31 @@ export default function Greet() {
     [greets]
   );
 
-  return <p className="font-bold text-4xl pb-4">{greetRandom}</p>;
+  return (
+    <div className="w-full flex justify-between items-center pb-4">
+      <p className="font-bold text-4xl">{greetRandom}</p>
+      <div
+        className="tooltip tooltip-left"
+        data-tip="Change profile"
+        onClick={() => GlobalContext.exitCurrentProfile()}
+      >
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 }
