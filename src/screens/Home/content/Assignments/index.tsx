b9a973d789d5ac0adca97cc5aa726a1fc10a6e79
neursh <31346263+neursh@users.noreply.github.com>
2025-10-31 10:07:46 +0700

    Handle state changes & animations

--- src/screens/Home/content/Assignments/index.tsx
+++ src/screens/Home/content/Assignments/index.tsx
@@ -6,16 +6,26 @@ import Greet from './Greet';
 
 export default function Assignments() {
   const selectedAssignment = useHookstate(HomeContext.selectedAssignment);
+  const selectedProblem = useHookstate(HomeContext.selectedProblem);
 
   return (
     <motion.section
       className="absolute top-4 bottom-4 overflow-scroll p-8 outline outline-[white]/40 rounded-2xl"
-      initial={{ width: 'calc(100% - 32px)' }}
+      initial={{
+        width: 'calc(100% - 32px)',
+        opacity: 1,
+        translateX: 'calc(0% - 0px)',
+      }}
       animate={{
         width:
           selectedAssignment.value !== ''
-            ? 'calc(60% - 40px)'
+            ? 'calc(55% - 40px)'
             : 'calc(100% - 32px)',
+        opacity: selectedAssignment.value !== '' ? 0.75 : 1,
+        translateX:
+          selectedProblem.value === ''
+            ? 'calc(0% - 0px)'
+            : 'calc(-100% - 32px)',
       }}
       transition={{ ease: [0.25, 0.25, 0.15, 1] }}
     >
--- src/screens/Home/content/Problems/index.tsx
+++ src/screens/Home/content/Problems/index.tsx
@@ -1,12 +1,16 @@
 import { useHookstate } from '@hookstate/core';
 import { motion } from 'motion/react';
 import { useLayoutEffect } from 'react';
-import { HomeContext } from '../../context';
+import { HomeContext, ProblemStatus } from '../../context';
+import CountdownTimer from '../CountdownTimer';
 
 export default function Problems() {
-  const problemsCache = useHookstate(HomeContext.problemsCache);
   const selectedAssignment = useHookstate(HomeContext.selectedAssignment);
   const assignments = useHookstate(HomeContext.assignments);
+  const selectedProblem = useHookstate(HomeContext.selectedProblem);
+  const problems = useHookstate(HomeContext.problems);
+
+  const showUp = useHookstate(false);
 
   useLayoutEffect(() => {
     if (selectedAssignment.value !== '') {
@@ -16,30 +20,103 @@ export default function Problems() {
 
   return (
     <motion.section
-      className="absolute w-[calc(40%-8px)] right-4 top-4 bottom-4 overflow-scroll p-8 outline outline-[white]/40 rounded-2xl"
+      className="absolute w-[calc(45%-8px)] right-4 top-4 bottom-4 backdrop-blur-2xl overflow-scroll outline outline-[white]/40 rounded-2xl"
       initial={{ translateX: 'calc(100% + 18px)' }}
       animate={{
         translateX:
-          selectedAssignment.value !== ''
-            ? 'calc(0% + 0px)'
-            : 'calc(100% + 18px)',
+          selectedAssignment.value === ''
+            ? 'calc(100% + 18px)'
+            : 'calc(0% + 0px)',
+        height: selectedProblem.value === '' ? '100%' : '50%',
+        translateY:
+          selectedProblem.value === ''
+            ? '0%'
+            : !showUp.value
+            ? 'calc(190% + 8px)'
+            : 'calc(100% + 8px)',
+        width:
+          selectedProblem.value === '' ? 'calc(45% - 8px)' : 'calc(30% - 8px)',
       }}
       transition={{
         ease: [0.25, 0.25, 0.15, 1],
+        duration: selectedProblem.value === '' ? 0.25 : 0.5,
+        width: {
+          duration: selectedProblem.value === '' ? 0.25 : 0.5,
+          delay: selectedProblem.value === '' ? 0 : 0.75,
+          ease: [0.25, 0.25, 0.15, 1],
+        },
+      }}
+      onClick={() => {
+        if (selectedProblem.value) {
+          showUp.set((p) => !p);
+        }
       }}
     >
-      <h1 className="text-2xl font-bold pb-4">
-        {selectedAssignment.value !== '' &&
-          assignments[selectedAssignment.value].value.name}
-      </h1>
-      {Object.keys(problemsCache.value).length !== 0 ? (
-        <>
-          {Object.entries(problemsCache.value).map((value) => (
-            <div key={`prob${value[0]}`}>{value[1].name}</div>
-          ))}
-        </>
+      <div className="sticky top-0 backdrop-blur-2xl flex flex-col p-8 outline outline-[white]/40 rounded-2xl z-10">
+        <div className="flex flex-wrap justify-between gap-2">
+          <h1 className="text-3xl font-bold text-nowrap overflow-hidden text-ellipsis">
+            {assignments[selectedAssignment.value].value?.name}
+          </h1>
+          <div className="flex flex-wrap gap-2">
+            <CountdownTimer
+              endDate={
+                assignments[selectedAssignment.value].value?.endDate ??
+                new Date()
+              }
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 pt-2">
+          <span className="badge badge-primary">Sort by status</span>
+          <span className="badge badge-secondary">Refresh</span>
+        </div>
+      </div>
+
+      {problems[selectedAssignment.value].value ? (
+        <div className="flex flex-col gap-4 p-8 pt-6">
+          {Object.entries(problems[selectedAssignment.value].value).map(
+            (value) => (
+              <div
+                key={`prob${value[0]}`}
+                className={`card cursor-pointer outline outline-[white]/80 ${
+                  value[1].status === ProblemStatus.solved
+                    ? 'outline-green-300 outline-2'
+                    : value[1].status === ProblemStatus.failed &&
+                      'outline-red-300 outline-2'
+                } ${selectedProblem.value === value[0] && 'outline-4'}`}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  if (selectedProblem.value !== value[0]) {
+                    selectedProblem.set(value[0]);
+                  } else {
+                    showUp.set(false);
+                    selectedProblem.set('');
+                  }
+                }}
+              >
+                <div className="card-body flex flex-row justify-between">
+                  <span className="text-lg font-bold">{value[1].name}</span>
+                  <span
+                    className={`badge ${
+                      value[1].status === ProblemStatus.solved
+                        ? 'badge-success'
+                        : value[1].status === ProblemStatus.failed
+                        ? 'badge-error'
+                        : 'badge-dash'
+                    }`}
+                  >
+                    {value[1].status !== ProblemStatus.noSubmission && '+'}
+                    {value[1].status !== ProblemStatus.failed
+                      ? value[1].score
+                      : '0'}
+                  </span>
+                </div>
+              </div>
+            )
+          )}
+        </div>
       ) : (
-        <div className="h-full flex justify-center items-center">
+        <div className="flex justify-center items-center pt-12">
           <span className="loading loading-spinner"></span>
         </div>
       )}
